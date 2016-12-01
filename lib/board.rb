3bde03570c9a5e5cfca468f02bd1d26d7f3058cf
Amaal Ali <amq.ali@gmail.com>
2016-12-01 14:06:47 +0000

    Guarding for invaild inputs.

--- lib/board.rb
+++ lib/board.rb
@@ -9,6 +9,23 @@ class Board
   end
 
   def play(symbol, row, column)
-
+    check_inputs(symbol, row, column)
   end
+
+  private
+
+  def check_inputs(symbol, row, column)
+    if symbol == nil or !['x', 'o'].include?(symbol)
+      raise ArgumentError.new("Symbol: You must enter an x or o")
+    end
+
+    if row == nil or ![1, 2, 3].include?(row)
+      raise ArgumentError.new("Row: You must enter a row in range 1-3")
+    end
+
+    if column == nil or ![1, 2, 3].include?(column)
+      raise ArgumentError.new("Column: You must enter a column in range 1-3")
+    end
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,7 @@
 describe Board do
-  describe 'initializtion' do
-    let(:board) { Board.new }
+  let(:board) { Board.new }
 
+  describe 'initializtion' do
     it 'an instance can be created' do
       expect(board).to be_instance_of(Board)
     end
@@ -12,6 +12,37 @@ describe Board do
   end
 
   describe '#play' do
+    context 'invalid inputs' do
+      it 'playing any symbol expect x or o results in an error message' do
+        expect {board.play('z',1,1)}.to raise_error(ArgumentError, 'Symbol: You must enter an x or o')
+      end
 
+      it 'specififying a row that is out of range' do
+        expect {board.play('x',4,1)}.to raise_error(ArgumentError, 'Row: You must enter a row in range 1-3')
+      end
+
+      it 'specififying a row that is not a number' do
+        expect {board.play('x','x',1)}.to raise_error(ArgumentError, 'Row: You must enter a row in range 1-3')
+      end
+
+      it 'specififying a column that is out of range' do
+        expect {board.play('x',1,4)}.to raise_error(ArgumentError, 'Column: You must enter a column in range 1-3')
+      end
+
+      it 'specififying a column that is not a number' do
+        expect {board.play('x',1,'x')}.to raise_error(ArgumentError, 'Column: You must enter a column in range 1-3')
+      end
+    end
+
+    # context 'valid inputs' do
+    #   it 'playing x in (1,1)' do
+    #     board.play('x',1,1)
+    #     expect(board.view).to eq([[x, nil, nil], [nil, nil, nil], [nil, nil, nil]])
+    #   end
+    #   it 'playing x in (1,1)' do
+    #     board.play('x',1,1)
+    #     expect(board.view).to eq([[x, nil, nil], [nil, nil, nil], [nil, nil, nil]])
+    #   end
+    # end
   end
 end
