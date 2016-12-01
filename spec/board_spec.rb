b0a0b22d2a428b3d19383a2b018e39cf605c30db
Amaal Ali <amq.ali@gmail.com>
2016-12-01 14:17:00 +0000

    Updated #play to check that the board has not been modified by invaild inputs.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,22 +15,27 @@ describe Board do
     context 'invalid inputs' do
       it 'playing any symbol expect x or o results in an error message' do
         expect {board.play('z',1,1)}.to raise_error(ArgumentError, 'Symbol: You must enter an x or o')
+        expect(board.view).to eq([[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]])
       end
 
       it 'specififying a row that is out of range' do
         expect {board.play('x',4,1)}.to raise_error(ArgumentError, 'Row: You must enter a row in range 1-3')
+        expect(board.view).to eq([[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]])
       end
 
       it 'specififying a row that is not a number' do
         expect {board.play('x','x',1)}.to raise_error(ArgumentError, 'Row: You must enter a row in range 1-3')
+        expect(board.view).to eq([[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]])
       end
 
       it 'specififying a column that is out of range' do
         expect {board.play('x',1,4)}.to raise_error(ArgumentError, 'Column: You must enter a column in range 1-3')
+        expect(board.view).to eq([[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]])
       end
 
       it 'specififying a column that is not a number' do
         expect {board.play('x',1,'x')}.to raise_error(ArgumentError, 'Column: You must enter a column in range 1-3')
+        expect(board.view).to eq([[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]])
       end
     end
 
