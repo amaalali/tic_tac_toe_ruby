e76b7696014ab866294e49dd88f639d663b48eb4
Amaal Ali <amq.ali@gmail.com>
2016-12-01 15:19:37 +0000

    Fixed Gameplay to use Players

--- lib/gameplay.rb
+++ lib/gameplay.rb
@@ -1,19 +1,18 @@
 class Gameplay
   def initialize(player_1, player_2)
-    @player_1 = player_1
-    @player_2 = player_2
-    @current_player = player_1
+    @players = [ Player.new(player_1,'X'), Player.new(player_2,'O') ]
+    @current_player = @players[0]
   end
 
   def player_1
-    @player_1.dup
+    @players[0].name
   end
 
   def player_2
-    @player_2.dup
+    @players[1].name.dup
   end
 
   def current_player
-    @current_player.dup
+    @current_player.name.dup
   end
 end
